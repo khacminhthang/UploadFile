0d39bc9575737232513cb3ea011df910d6206b2f
Le Minh Thang <thanglkm@vnpt.vn>
2022-02-11 10:43:16 +0700

    Initial Commit

---
+++ UploadFile/Models/MultipleFilesModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UploadFile.Models
+{
+    public class MultipleFilesModel : ResponseModel
+    {
+        [Required(ErrorMessage = "Please select files")]
+        public List<IFormFile> Files { get; set; }
+    }
+}
---
+++ UploadFile/Models/SingleFileModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace UploadFile.Models
+{
+    public class SingleFileModel : ResponseModel
+    {
+        [Required(ErrorMessage = "Please enter file name")]
+        public string FileName { get; set; }
+        [Required(ErrorMessage = "Please select file")]
+        public IFormFile File { get; set; }
+
+    }
+}
